e52998ad83955a08278b3d09d9f1137ee18ace2b
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2025-09-21 20:52:01 +0700

    Update logger code

--- Assets/Commons/DebugUtils/Scripts/LoggerUtils.cs
+++ Assets/Commons/DebugUtils/Scripts/LoggerUtils.cs
@@ -1,14 +1,11 @@
-using System.Diagnostics;
 using System.Collections.Generic;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 namespace PracticalUtilities.DebugUtils.Scripts
 {
     public static class LoggerUtils
     {
         private static HashSet<string> _logChannels;
-        private const string ConditionalAttribute = "USE_LOGGER_UTILS";
 
         public static void Initialize(string channelCollectionPath)
         {
@@ -18,22 +15,19 @@ namespace PracticalUtilities.DebugUtils.Scripts
             for (int i = 0; i < logChannels.channels.Length; i++)
                 _logChannels.Add(logChannels.channels[i]);
         }
-
-        [Conditional(ConditionalAttribute)]
+        
         public static void Log(string message, string channel = null)
         {
             string logInfo = IsAvailableChannel(channel) ? $"[{channel}]{message}" : message;
             Debug.Log(logInfo);
         }
         
-        [Conditional(ConditionalAttribute)]
         public static void LogWarning(string message, string channel = null)
         {
             string logInfo = IsAvailableChannel(channel) ? $"[{channel}]{message}" : message;
             Debug.LogWarning(logInfo);
         }
         
-        [Conditional(ConditionalAttribute)]
         public static void LogError(string message, string channel = null)
         {
             string logInfo = IsAvailableChannel(channel) ? $"[{channel}]{message}" : message;
