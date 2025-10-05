17baf2601c372cb9b33943e5066d56917dbea908
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2025-10-05 21:05:49 +0700

    Update code

--- Assets/PracticalUtilities/CalculationExtensions/PhysicsExtensions.cs
+++ Assets/PracticalUtilities/CalculationExtensions/PhysicsExtensions.cs
@@ -8,11 +8,12 @@ namespace PracticalUtilities.CalculationExtensions
             rigidbody.maxLinearVelocity = maxMagnitude;
 
         public static Vector3 GetFlatVelocity(this Rigidbody rigidbody) =>
-            rigidbody.linearVelocity.x * Vector3.right + rigidbody.linearVelocity.z * Vector3.forward;
+            (rigidbody.linearVelocity.x * Vector3.right).FasterAdd(rigidbody.linearVelocity.z * Vector3.forward);
 
         public static float GetSquaredFlatSpeed(this Rigidbody rigidbody)
         {
-            Vector3 flatVelocity = rigidbody.linearVelocity.x * Vector3.right + rigidbody.linearVelocity.z * Vector3.forward;
+            Vector3 flatVelocity =
+                (rigidbody.linearVelocity.x * Vector3.right).FasterAdd(rigidbody.linearVelocity.z * Vector3.forward);
             return flatVelocity.sqrMagnitude;
         }
 
