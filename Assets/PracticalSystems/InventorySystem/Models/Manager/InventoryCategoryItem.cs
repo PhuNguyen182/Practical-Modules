9f685a1029bb51808f9e12c40c7045b1194bb250
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2025-11-02 16:28:19 +0700

    Split code

---
+++ Assets/PracticalSystems/InventorySystem/Models/Manager/InventoryCategoryItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticalSystems.InventorySystem.Models.Manager
+{
+    [Serializable]
+    public class InventoryCategoryItem
+    {
+        public Dictionary<string, InventoryItem> ItemData = new();
+
+        public void AddItem(InventoryItem item)
+        {
+            string itemId = item.itemId;
+            if (this.ItemData.TryAdd(itemId, item)) 
+                return;
+            
+            var existingItem = this.ItemData[itemId];
+            existingItem.quantity += item.quantity;
+            this.ItemData[itemId] = existingItem;
+        }
+        
+        public bool RemoveItem(string itemId, int quantity = 1, bool forceRemove = false)
+        {
+            if (!this.ItemData.TryGetValue(itemId, out InventoryItem item))
+                return false;
+
+            if (forceRemove)
+            {
+                this.ItemData.Remove(itemId);
+                return true;
+            }
+            
+            int currentQuantity = item.quantity;
+            int offset = currentQuantity - quantity;
+            
+            if (offset > 0)
+            {
+                item.quantity = offset;
+                this.ItemData[itemId] = item;
+            }
+            else
+            {
+                this.ItemData.Remove(itemId);
+            }
+            
+            return true;
+        }
+    }
+}
